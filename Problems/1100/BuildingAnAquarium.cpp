cfe15b7ae527ad751ffe80d5ff72bcde33088c33
DrewMiller2024 <drew.miller@mrjaffesclass.com>
2023-11-21 18:58:12 -0800

    rearranged everything into contests and problems folders

---
+++ Problems/1100/BuildingAnAquarium.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+
+int32_t main() {
+	cin.tie(NULL); cout.tie(NULL);
+  ll t;
+  cin >> t;
+  while (t--) {
+    
+  }
+  return 0;
+}
