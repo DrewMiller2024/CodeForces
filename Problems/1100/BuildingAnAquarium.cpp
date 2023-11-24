57e58b309ceb46ceb3a82af7afb8b143267e465a
DrewMiller2024 <drew.miller@mrjaffesclass.com>
2023-11-24 00:28:00 -0800

    BuildingAnAquarium

--- Problems/1100/BuildingAnAquarium.cpp
+++ Problems/1100/BuildingAnAquarium.cpp
@@ -10,7 +10,35 @@ int32_t main() {
   ll t;
   cin >> t;
   while (t--) {
-    
+    ll n, x;
+    cin >> n >> x;
+    vll corals;
+    for (ll i = 0; i < n; i++) {
+      ll temp;
+      cin >> temp;
+      corals.push_back(temp);
+    }
+    sort(corals.begin(), corals.end());
+    ll l = 0;
+    ll r = INT32_MAX;
+    ll m;
+    while (l < r) {
+      m = l + ((r - l + 1) / 2);
+      ll sum = 0;
+      for (ll i = 0; i < n; i++) {
+        if (m - corals[i] > 0) {
+          sum += m - corals[i];
+        } else {
+          break;
+        }
+      }
+      if (sum <= x) {
+        l = m;
+      } else {
+        r = m - 1;
+      }
+    }
+    cout << l << endl;
   }
   return 0;
 }
